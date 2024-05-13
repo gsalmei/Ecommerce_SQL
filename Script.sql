e746700e6aec25561b0eabc6992947fd8ee5f8fd
Gustavo Almeida <134868461+gsalmei@users.noreply.github.com>
2024-05-13 16:53:45 -0300

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,4 +1,4 @@
-"1. Buscando produtos mais vendidos:"
+-- Buscando os produtos mais vendidos em quantidade
 
 SELECT
 	opd.product_category_name,
@@ -17,3 +17,24 @@ GROUP BY
 	opd.product_category_name 
 ORDER BY 
 	qtd_pedidos DESC
+	
+-- Buscando os produtos com maior receita
+
+SELECT 
+	opd.product_category_name,
+	sum(ooid.price) as preco_pedidos 
+FROM 
+	olist_orders_dataset ood
+LEFT JOIN olist_order_items_dataset ooid 
+ON
+	ood.order_id = ooid.order_id 
+LEFT JOIN olist_products_dataset opd 
+ON
+	ooid.product_id = opd.product_id 
+WHERE 
+	opd.product_category_name <> ''
+GROUP BY 
+	opd.product_category_name 
+ORDER BY 
+	preco_pedidos DESC 
+	
